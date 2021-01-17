339e7b8650c33de60a41ee74ce9a01bc2b1270fa
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2021-01-17 21:38:58 +0900

    added contact routing

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('tests/test','TestController@index');
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function(){
+    Route::get('index','ContactFormController@index')->name('contact.index');
+});
+
+
+// Route::get('contact/index','ContactFormController@index');
+
 
 // Route::resource('contacts','ContactFormController');
 
-// Auth::routes();
+Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
