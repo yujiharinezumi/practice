878469a16d881556f0790d975ccab9c1d816011b
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2021-01-17 21:13:42 +0900

    created contac controller & model

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 
 Route::get('tests/test','TestController@index');
-Auth::routes();
+
+// Route::resource('contacts','ContactFormController');
+
+// Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
