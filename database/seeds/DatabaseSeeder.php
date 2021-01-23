3357562947088ee30775fec8d4f44c704fe9bd3a
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2021-01-23 15:29:13 +0900

    added seed data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,7 +10,8 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    { 
+        $this->call(UsersTableSeeder::class);
+        $this->call(ContactFormSeeder::class);
     }
 }
