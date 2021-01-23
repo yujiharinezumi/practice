83d5019d871d7609d346f03734ad20fe8961f80c
yuji sugaya <yujibebobe.jiangu.yousi@gmail.com>
2021-01-23 15:35:40 +0900

    fixed layouts

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -14,25 +14,22 @@
                         </div>
                     @endif
 
-                    showです
-                    {{ $contact->your_name}}
-                    {{ $contact->title}}
-                    {{ $contact->email}}
-                    {{ $contact->url}}
-                    {{ $gender}}
-                    {{ $age}}
-                    {{ $contact->contact}}
+                    <p>氏名:{{ $contact->your_name}}</p>
+                    <p>件名:{{ $contact->title}}</p>
+                    <p>メールアドレス:{{ $contact->email}}</p>
+                    <p>URL:{{ $contact->url}}</p>
+                    <p>性別:{{ $gender}}</p>
+                    <p>年齢:{{ $age}}</p>
+                    <p>お問い合わせ内容</p>
+                    <p>{{ $contact->contact}}</p>
                     <form method="GET" action="{{ route('contact.edit',['id'=> $contact->id ]) }}">
                     @csrf
-                     
-
                     <input class="btn btn-info" type="submit" value="変更する">
-
                     </form>
-
+                    <br>
                     <form method="POST" action="{{ route('contact.destroy',['id'=> $contact->id ]) }}" id="delete_{{ $contact->id }}">
                     @csrf 
-                    <a href="#" class="btn btn-danger" data-id="{{ $contact->id}}" onclick="deletePost(this);">削除する</a>
+                    <p><a href="#" class="btn btn-danger" data-id="{{ $contact->id}}" onclick="deletePost(this);">削除する</a></p>
                     </form>
 
                 
